d0caa60e4ca283e8129b359b85dacaba5b8a76be
Troy Harris <2343470+troyharris@users.noreply.github.com>
2023-08-25 16:01:48 -0700

    Added authentication. Added requirements file.

--- hello.py
+++ hello.py
@@ -1,13 +1,49 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import openai
 import os
-import markdown
+#import markdown
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Tells flask-sqlalchemy what database to connect to
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+# Enter a secret key
+app.config["SECRET_KEY"] = os.environb[b'FLASK_SECRET_KEY']
+# Initialize flask-sqlalchemy extension
+db = SQLAlchemy()
+
+# LoginManager is needed for our application
+# to be able to log in and out users
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Create user model
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True,
+                         nullable=False)
+    password = db.Column(db.String(250),
+                         nullable=False)
+    
+# Initialize app with extension
+db.init_app(app)
+# Create database within app context
+ 
+with app.app_context():
+    db.create_all()
+
+# Creates a user loader callback that returns the user object given an id
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+
 @app.route('/')
 def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for("login"))
     return render_template('index.html')
 
 @app.route('/get_response', methods=['POST'])
@@ -47,8 +83,23 @@ def get_response():
     )
     print(response)
     return jsonify(response)
-    #if system_prompt_code == "6":
-    #    return markdown.markdown(response.choices[0].message["content"], extensions=['fenced_code'])
-    #else:
-    #    return jsonify(response)
-    #    return response.choices[0].message["content"]
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # If a post request was made, find the user by
+    # filtering for the username
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        # Check if the password entered is the
+        # same as the user's password
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("index"))
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
