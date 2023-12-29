b711e03ae57d8d9e6556f3d9eed95d5a3ac3c0aa
Troy Harris <2343470+troyharris@users.noreply.github.com>
2023-12-29 13:43:13 -0700

    Some Google auth cleanup, comment cleanup, and refactoring

--- hello.py
+++ hello.py
@@ -10,12 +10,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_migrate import Migrate
 from authlib.integrations.flask_client import OAuth
-#from flask_cors import CORS
-
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-#CORS(app)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Configuration for OAuth
@@ -40,8 +36,8 @@ google = oauth.register(
     api_base_url=google_discovery['userinfo_endpoint'],
     client_kwargs={'scope': 'openid email profile'},
     userinfo_endpoint=google_discovery['userinfo_endpoint'],
-    jwks_uri=google_discovery['jwks_uri'],  # Set the jwks_uri, this should prevent the error
-    server_metadata_url=google_discovery_url,   # This will tell Authlib to use the discovery document
+    jwks_uri=google_discovery['jwks_uri'],
+    server_metadata_url=google_discovery_url,
 )
 
 # Tells flask-sqlalchemy what database to connect to
@@ -54,12 +50,11 @@ db = SQLAlchemy()
 # Initialize Flask-Migrate
 migrate = Migrate(app, db)
 
-# LoginManager is needed for our application
-# to be able to log in and out users
+# Initialize LoginManager for handling user authentication
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# Create user model
+# Define User model
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=True, nullable=False)
@@ -67,6 +62,7 @@ class Users(UserMixin, db.Model):
     email = db.Column(db.String(250), nullable=True)
     is_admin = db.Column(db.Integer, nullable=True)
 
+#Define ConversationHistory model (for storing conversations)
 class ConversationHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -78,13 +74,13 @@ class ConversationHistory(db.Model):
     def __repr__(self):
         return f'<ConversationHistory {self.id}>'
     
+    # Get a dict of the ConversationHistory object
     def toDict(self):
        return dict(id=self.id, title=self.title, conversation=self.conversation)
     
-# Initialize app with extension
+# Initialize SQLAlchemy with app
 db.init_app(app)
-# Create database within app context
- 
+# Create database within app context if needed
 with app.app_context():
     db.create_all()
 
@@ -93,9 +89,14 @@ with app.app_context():
 def loader_user(user_id):
     return Users.query.get(user_id)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# Set which extentions are allowed for upload. NOTE: Not implemented
+#
+#ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+#
+# Manage allowed 
+#def allowed_file(filename):
+#    return '.' in filename and \
+#           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -112,11 +113,8 @@ def get_response():
     system_prompt_code = request_json["system-prompt"]
     image_data = request_json["imageData"]
     model = request_json["model"]
-    #prompt = request.form['prompt']
-    #if len(response_history) == 1 :
-    prompt = "hello there?"
-    #system_prompt_code = 1
-    #system_prompt_code = request.form['system-prompt']
+
+    # Translate system prompt id to system instructions for GPT
     system_prompt = "You are a helpful assistant."
     if system_prompt_code == "2":
         system_prompt = "You are an expert scientist and will answer questions in accurate but simple to understand terms"
@@ -132,19 +130,19 @@ def get_response():
         system_prompt = "You are an expert at composing emails. You write your emails using proper grammar and punctuation. Your tone is friendly and professional but not overly formal."
     if system_prompt_code != "6":
         system_prompt = system_prompt + " Format your response as HTML using Bootstrap 5 HTML tags and code. Use hyperlinks to link to resources but only if helpful and possible. Don't use Markdown or wrap your response in markdown. Don't use ``` tags."
-    print(system_prompt)
+ 
+    # Create the message object and populate it with any chat history
     messages = [{"role": "system", "content": system_prompt}]
     messages += response_history
-    print(messages)
-    ### If a file was uploaded, load the vision model no matter what and override the system prompt ###
+
+    # If a file was uploaded, load the vision model no matter what and override the system prompt
     if image_data and image_data != '':
         print("I found a file")
-        #file = request.files['file']
-        #base64_image = base64.b64encode(file.read())
+        prompt = request.form['prompt']
         content = [
             {
                 "type": "text",
-                "text": "What is in this image?"
+                "text": prompt
             },
             {
                 "type": "image_url",
@@ -155,8 +153,8 @@ def get_response():
         system_prompt = 'You are a helpful assistant that can describe an image in detail.'
         messages = [{"role": "system", "content": system_prompt}]
         messages += [{"role": "user", "content": content}]
-    else:
-        print("I did not find a file")
+    
+    # If DALL-E 3 was selected, we use a different type of API call than the others
     if model == 'dall-e-3':
         print("generate image")
         image_prompt = request_json["imagePrompt"]
@@ -167,30 +165,19 @@ def get_response():
             quality="standard",
             n=1
         )
-        image_url = response.data[0].url
-        print("Image URL....")
-        print(image_url)
-        print("End URL")
+        # DALL-E-3 returns a response that includes an image URL. The front-end knows what to do with it.
         return jsonify(response)
     else:
+        # All models besides DALL-E 3 use the ChatCompletion API call
         response =  openai.ChatCompletion.create(
             #model="gpt-4-1106-preview", 
             model=model,
             messages=messages,
             max_tokens=4096
         )
-        print(response)
-        response_json = jsonify(response)
-        
-        #conversation_history_entry = ConversationHistory(
-        #    user_id=current_user.id,
-        #    conversation=response_json.get_data(as_text=True)  # Or however you want to format the content
-        #)
-        #db.session.add(conversation_history_entry)
-        #db.session.commit()
-        
-        return response_json
+        return jsonify(response)
 
+# Routes to retrieve saved conversations
 @app.route('/history', methods=['GET'])
 @login_required
 def history():
@@ -204,27 +191,26 @@ def single_history(id):
     conversation_json = jsonify(conversation.toDict())
     return conversation_json
 
+# Route to save a conversation
 @app.route('/add_conversation', methods=['POST'])
 @login_required
 def add_conversation():
     request_json = request.get_json()
-    #print(request_json)
-    #response_history = request_json["responseHistory"]
     conversation_json = jsonify(request_json)
     conversation_json_string = conversation_json.get_data(as_text=True)
 
+    # Ask ChatGPT to summerize the conversation as a single sentence and use for the conversation title.
     messages = [{"role": "system", "content": "You are an expert at taking in OpenAI API JSON chat requests and coming up with a brief one sentance title for the chat history."}]
     prompt = "Give me a short, one sentence title for this chat history: " + conversation_json_string
     messages += [{"role": "user", "content": prompt}]
-
     response =  openai.ChatCompletion.create(
         model="gpt-3.5-turbo", 
         messages=messages,
-        max_tokens=2048
+        max_tokens=4096
     )
-
     title = response.choices[0].message.content
 
+    # Save the conversation
     conversation_history_entry = ConversationHistory(
         user_id=current_user.id,
         title = title,
@@ -234,6 +220,7 @@ def add_conversation():
     db.session.commit()
     return request_json
 
+# Google authentication
 @app.route('/login/google', methods=["GET", "POST"])
 def google_login():
     redirect_uri = url_for('google_authorize', _external=True, _scheme="https")
@@ -245,10 +232,9 @@ def google_authorize():
     token = google.authorize_access_token()
     # You can use the token to get user info
     resp = google.get('userinfo', token=token)
+    resp.raise_for_status
     user_info = resp.json()
-    # Implement the logic to handle the response such as logging in the user, creating the user, etc.
-    
-    # For example, find or create a user
+
     user = Users.query.filter_by(email=user_info['email']).first()
     if not user:
         password = generate_random_password()
@@ -261,7 +247,7 @@ def google_authorize():
     login_user(user)
     return redirect('/')
 
-
+# Flask-login to login user
 @app.route("/login", methods=["GET", "POST"])
 def login():
     # If a post request was made, find the user by
@@ -277,12 +263,14 @@ def login():
             return redirect(url_for("index"))
     return render_template("login.html")
 
+# Flask-login to log out user
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("index"))
 
+# Settings page
 @app.route('/settings')
 @login_required
 def settings():
@@ -292,6 +280,7 @@ def settings():
     users = Users.query.all()
     return render_template('settings.html', users=users)
 
+# Add a user from the settings page
 @app.route("/settings/add_user", methods=["POST"])
 @login_required
 def add_user():
@@ -306,6 +295,7 @@ def add_user():
 
     return redirect(url_for("settings"))
 
+# Delete a user from the settings page
 @app.route("/settings/delete_user/<int:id>", methods=["POST"])
 @login_required
 def delete_user(id):
@@ -316,6 +306,7 @@ def delete_user(id):
 
     return redirect(url_for("settings"))
 
+# WARNING! For dev purposes only. Will be removed in a future update. Will delete all saved conversations for all users
 @app.route("/history/delete_all", methods=["GET", "POST"])
 @login_required
 def delete_all():
@@ -324,6 +315,7 @@ def delete_all():
 
     return redirect(url_for("history"))
 
+# When an account is created via Google, a password still needs to be created but doesn't need to be used.
 def generate_random_password():
     chars = string.ascii_letters + string.digits + '+/'
     assert 256 % len(chars) == 0  # non-biased later modulo
