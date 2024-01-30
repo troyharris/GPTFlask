d5d9b3b87cee4688371b8c9dac49291ff732cc9c
Troy Harris <2343470+troyharris@users.noreply.github.com>
2024-01-29 20:05:47 -0700

    Removed old non-API code & login functionality

--- snowgoose.py
+++ snowgoose.py
@@ -1,16 +1,13 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, abort
+from flask import Flask, render_template, request, jsonify, abort
 import requests
 import openai
 import os
-import base64
 import json
 import string
 from datetime import datetime
 from os import environ
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_migrate import Migrate
-from authlib.integrations.flask_client import OAuth
 from functools import wraps, partial
 from flask_cors import CORS
 
@@ -20,32 +17,9 @@ CORS(app)
 # Get OpenAI's API key from the .env file
 openai.api_key = environ.get("OPENAI_API_KEY")
 
-# Configuration for Google OAuth
-app.config["GOOGLE_CLIENT_ID"] = environ.get("GOOGLE_CLIENT_ID")
-app.config["GOOGLE_CLIENT_SECRET"] = environ.get("GOOGLE_CLIENT_SECRET")
 # Ensure SESSION_COOKIE_SECURE is True in production
 app.config["SESSION_COOKIE_SECURE"] = environ.get("SESSION_COOKIE_SECURE")
 app.config["SESSION_PERMANENT"] = False
-
-
-# Get the Google OpenID configuration
-google_discovery_url = "https://accounts.google.com/.well-known/openid-configuration"
-google_discovery = requests.get(google_discovery_url).json()
-
-# Initialize OAuth extension
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id=app.config["GOOGLE_CLIENT_ID"],
-    client_secret=app.config["GOOGLE_CLIENT_SECRET"],
-    access_token_url=google_discovery['token_endpoint'],
-    authorize_url=google_discovery['authorization_endpoint'],
-    api_base_url=google_discovery['userinfo_endpoint'],
-    client_kwargs={'scope': 'openid email profile'},
-    userinfo_endpoint=google_discovery['userinfo_endpoint'],
-    jwks_uri=google_discovery['jwks_uri'],
-    server_metadata_url=google_discovery_url,
-)
 
 # Grabs PostGreSQL info and connects
 dbname = environ.get("POSTGRES_DB")
@@ -64,14 +38,9 @@ db = SQLAlchemy()
 # Initialize Flask-Migrate
 migrate = Migrate(app, db, render_as_batch=True)
 
-# Initialize LoginManager for handling user authentication
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
 # Models
 # User model
-class Users(UserMixin, db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
@@ -119,6 +88,15 @@ class Persona(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
     owner = db.relationship('Users', backref=db.backref('personas', lazy=True))
 
+    def toDict(self):
+        persona_obj = {
+            "id": self.id,
+            "name": self.name,
+            "prompt": self.prompt,
+            "owner_id": self.owner_id
+        }
+        return persona_obj
+
 # Output format model
 class OutputFormat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -156,11 +134,6 @@ db.init_app(app)
 # Create database within app context if needed
 with app.app_context():
     db.create_all()
-
-# Creates a user loader callback that returns the user object given an id
-@login_manager.user_loader
-def loader_user(user_id):
-    return Users.query.get(user_id)
 
 # Set which extentions are allowed for upload. NOTE: Not implemented
 #
@@ -305,70 +278,7 @@ def openai_request(request):
 # Routing
 @app.route('/')
 def index():
-    if not current_user.is_authenticated:
-        print("current user is not authenticated")
-        return redirect(url_for("login"))
-    
-    history = ConversationHistory.query.filter_by(user_id=current_user.id).order_by(ConversationHistory.timestamp.desc()).all()
-    personas = Persona.query.all()
-    output_formats = OutputFormat.query.all()
-    return render_template('index.html', history=history, personas=personas, output_formats=output_formats)
-
-@app.route('/chat', methods=['POST'])
-@require_api_key
-def chat():
-    request_dict = openai_request(request)
-
-    # If a file was uploaded, load the vision model no matter what and override the system prompt
-    if request_dict["image_data"] and request_dict["image_data"] != '':
-        print("I found a file")
-        prompt = request_dict['prompt']
-        image_data = request_dict["image_data"]
-        content = [
-            {
-                "type": "text",
-                "text": prompt
-            },
-            {
-                "type": "image_url",
-                "image_url": f"{image_data}"
-            }
-        ]
-        request_dict["model"]='gpt-4-vision-preview'
-        system_prompt = 'You are a helpful assistant that can describe an image in detail.'
-        messages = [{"role": "system", "content": system_prompt}]
-        messages += [{"role": "user", "content": content}]
-        request_dict["messages"] = messages
-
-        response =  openai.ChatCompletion.create(
-            model=request_dict["model"],
-            messages=request_dict["messages"],
-            max_tokens=1024
-        )
-        return jsonify(response)
-
-    # If just a standard chat model, we simply pass it our model and messages object
-    else: 
-        response =  openai.ChatCompletion.create(
-            model=request_dict["model"],
-            messages=request_dict["messages"]
-        )
-        return jsonify(response)
-    
-
-@app.route('/dalle', methods=['POST'])
-def dalle():
-    request_dict = openai_request(request)
-    prompt = request_dict["prompt"]
-    response = openai.Image.create(
-        model="dall-e-3",
-        prompt=prompt,
-        size="1024x1024",
-        quality="standard",
-        n=1
-    )
-    # DALL-E-3 returns a response that includes an image URL. The front-end knows what to do with it.
-    return jsonify(response)
+    return render_template('index.html')
 
 @app.route('/api/test', methods=['POST'])
 # @require_api_key
@@ -454,7 +364,7 @@ def api_output_formats():
     #if not current_user.is_admin:
     #    return redirect(url_for('index'))
     
-    output_formats = OutputFormat.query.all()
+    output_formats = OutputFormat.query.order_by(OutputFormat.id).all()
     # personas_json = json.dumps([ob.__dict__ for ob in personas])
     return output_formats_json(output_formats)
 
@@ -587,219 +497,6 @@ def api_delete_history(id, user):
         print(e)
         return jsonify({"message": "An unexpected error occurred."}), 500
 
-# Routes to retrieve saved conversations
-@app.route('/history', methods=['GET'])
-@login_required
-def history():
-    history = ConversationHistory.query.filter_by(user_id=current_user.id).order_by(ConversationHistory.timestamp.desc()).all()
-    return render_template('history.html', history=history)
-
-@app.route('/history/id/<int:id>', methods=['POST'])
-@login_required
-def single_history(id):
-    conversation = ConversationHistory.query.get(id)
-    conversation_json = jsonify(conversation.toDict())
-    return conversation_json
-
-# Route to save a conversation
-@app.route('/add_conversation', methods=['POST'])
-@login_required
-def add_conversation():
-    request_json = request.get_json()
-    conversation_json = jsonify(request_json)
-    conversation_json_string = conversation_json.get_data(as_text=True)
-
-    # Ask ChatGPT to summerize the conversation as a single sentence and use for the conversation title.
-    messages = [{"role": "system", "content": "You are an expert at taking in OpenAI API JSON chat requests and coming up with a brief one sentance title for the chat history."}]
-    prompt = "Give me a short, one sentence title for this chat history: " + conversation_json_string
-    messages += [{"role": "user", "content": prompt}]
-    response =  openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", 
-        messages=messages
-    )
-    title = response.choices[0].message.content
-
-    # Save the conversation
-    conversation_history_entry = ConversationHistory(
-        user_id=current_user.id,
-        title = title,
-        conversation=conversation_json_string  # Or however you want to format the content
-    )
-    db.session.add(conversation_history_entry)
-    db.session.commit()
-    return request_json
-
-# Google authentication
-@app.route('/login/google', methods=["GET", "POST"])
-def google_login():
-    redirect_uri = url_for('google_authorize', _external=True, _scheme="https")
-    return google.authorize_redirect(redirect_uri)
-
-@app.route('/login/google/authorize', methods=["GET", "POST"])
-def google_authorize():
-    # Get the authorization token
-    token = google.authorize_access_token()
-    # You can use the token to get user info
-    resp = google.get('userinfo', token=token)
-    resp.raise_for_status
-    user_info = resp.json()
-
-    user = Users.query.filter_by(email=user_info['email']).first()
-    if not user:
-        password = generate_random_password()
-        user = Users(username=user_info['name'], password=password, email=user_info['email'])
-        db.session.add(user)
-        db.session.commit()
-    
-    # Login the user with Flask-Login
-    login_user(user)
-    return redirect('/')
-
-# Flask-login to login user
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    # If a post request was made, find the user by
-    # filtering for the username
-    if request.method == "POST":
-        print("Getting the user")
-        user = Users.query.filter_by(
-            username=request.form.get("username")).first()
-        # Check if the password entered is the
-        # same as the user's password
-        if user.password == request.form.get("password"):
-            print("User's password is correct")
-            # Use the login_user method to log in the user
-            login_user(user)
-            return redirect(url_for("index"))
-        else:
-            print("invalid login")
-    else:
-        print("login method was not POST")
-    return render_template("login.html")
-
-# Flask-login to log out user
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("index"))
-
-# Settings page
-@app.route('/settings')
-@login_required
-def settings():
-    if not current_user.is_admin:
-        return redirect(url_for('index'))
-    
-    users = Users.query.all()
-    return render_template('settings.html', users=users)
-
-# Add a user from the settings page
-@app.route("/settings/add_user", methods=["POST"])
-@login_required
-def add_user():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    email = request.form.get("email")
-    is_admin = request.form.get("is_admin")
-
-    new_user = Users(username=username, password=password, email=email, is_admin=is_admin)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for("settings"))
-
-# Delete a user from the settings page
-@app.route("/settings/delete_user/<int:id>", methods=["POST"])
-@login_required
-def delete_user(id):
-    user = Users.query.get(id)
-
-    db.session.delete(user)
-    db.session.commit()
-
-    return redirect(url_for("settings"))
-
-# WARNING! For dev purposes only. Will be removed in a future update. Will delete all saved conversations for all users
-@app.route("/history/delete_all", methods=["GET", "POST"])
-@login_required
-def delete_all():
-    db.session.query(ConversationHistory).delete()
-    db.session.commit()
-
-    return redirect(url_for("history"))
-
-# Personas page
-@app.route('/personas')
-@login_required
-def personas():
-    if not current_user.is_admin:
-        return redirect(url_for('index'))
-    
-    personas = Persona.query.all()
-    return render_template('personas.html', personas=personas)
-
-# Add a persona from the personas page
-@app.route("/personas/add_persona", methods=["POST"])
-@login_required
-def add_persona():
-    name = request.form.get("name")
-    prompt = request.form.get("prompt")
-    owner = request.form.get("owner")
-
-    new_persona = Persona(name=name, prompt=prompt)
-    db.session.add(new_persona)
-    db.session.commit()
-
-    return redirect(url_for("personas"))
-
-# Delete a user from the settings page
-@app.route("/personas/delete_persona/<int:id>", methods=["POST"])
-@login_required
-def delete_persona(id):
-    persona = Persona.query.get(id)
-
-    db.session.delete(persona)
-    db.session.commit()
-
-    return redirect(url_for("personas"))
-
-# Output Format page
-@app.route('/output_formats')
-@login_required
-def output_formats():
-    if not current_user.is_admin:
-        return redirect(url_for('index'))
-    
-    output_formats   = OutputFormat.query.all()
-    return render_template('output_formats.html', output_formats=output_formats)
-
-# Add an output format from the output formats page
-@app.route("/output_formats/add_output_format", methods=["POST"])
-@login_required
-def add_output_format():
-    name = request.form.get("name")
-    prompt = request.form.get("prompt")
-    owner = request.form.get("owner")
-
-    new_output_format = OutputFormat(name=name, prompt=prompt)
-    db.session.add(new_output_format)
-    db.session.commit()
-
-    return redirect(url_for("output_formats"))
-
-# Delete an output format from the output formats page
-@app.route("/output_formats/delete_output_format/<int:id>", methods=["POST"])
-@login_required
-def delete_output_format(id):
-    output_format = OutputFormat.query.get(id)
-
-    db.session.delete(output_format)
-    db.session.commit()
-
-    return redirect(url_for("output_formats"))
-
-
 # When an account is created via Google, a password still needs to be created but doesn't need to be used.
 def generate_random_password():
     chars = string.ascii_letters + string.digits + '+/'
@@ -812,24 +509,9 @@ def generate_random_password():
 def personas_json(personas):
     persona_array = []
     for persona in personas:
-        persona_obj = {
-            "id": persona.id,
-            "name": persona.name,
-            "prompt": persona.prompt,
-            "owner_id": persona.owner_id
-        }
+        persona_obj = persona.toDict()
         persona_array.append(persona_obj)
     return json.dumps(persona_array)
-
-def models():
-    models = [
-        {"name": "gpt-4-turbo-preview", "title": "New GPT-4 Turbo"},
-        {"name": "gpt-4-1106-preview", "title": "Old GPT-4 Turbo"},
-        {"name": "gpt-4", "title": "GPT-4 Classic"},
-        {"name": "gpt-3.5-turbo", "title": "GPT-3.5 Turbo"},
-        {"name": "dall-e-3", "title": "DALL-E 3"}
-    ]
-    return json.dumps(models)
 
 def models_json(models):
     models_array = []
